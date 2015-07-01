4cf6afcc0e0e128e119ec26ed5a75510bfb8a3c3
Adam Breaux <breauxa@email.chop.edu>
2015-07-01 10:29:22 -0400

    dep hell

--- manifests/init.pp
+++ manifests/init.pp
@@ -6,18 +6,8 @@ class phpredis {
     ],
   }
 
-  $depends = $::operatingsystem ? {
-    /(?i-mx:centos|fedora|redhat|scientific)/ => [ 'php' ],
-  }
-
-  $service = $::operatingsystem ? {
-    /(?i-mx:centos|fedora|redhat|scientific)/ => [ 'httpd' ],
-  }
-
   package { $required:
     ensure  => latest,
-    require => Package[$depends],
-    notify  => Service[$service],
   }
 
 }
